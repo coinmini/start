d240989ccde364dc2c91604319a9d9a87152dcd8
JJ <coinmini@gmail.com>
2023-02-11 01:10:51 +0800

    Create git_fetch.sh

---
+++ git_fetch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git fetch
+LOCAL=$(git log -1 --pretty=format:"%H")
+REMOTE=$(git log remotes/origin/main -1 --pretty=format:"%H")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+else
+    echo "Need update"
+    git pull
+    /bin/cp -rf /root/start/start.sh /root/start/test/
+    chmod +x /root/start/test/start.sh
+fi
